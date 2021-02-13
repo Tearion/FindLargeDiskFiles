60df2b0d2a9de1262614df939cddf3e9321b73ae
r3n <robin.ebeling@gmx.net>
2021-02-13 15:16:48 +0100

    Implemented input parameter. Refactored start.py. Added -h parameter.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # FindLargeDiskFiles
 A small python demo, that collects large filenames and write them into a text file.
-It should help to identify large unsused files that increase your disk usage.
+It should help to identify large unused files that increase your disk usage.
 
-Folders like `C:/User/myUser/AppData` are growing over the years.
+Folders like `C:\User\myUser\AppData` are growing over the years.
+
+Example result:
 `Size of the folder including subfolders (C:\Users\myuser\AppData) is 34433 MB
 Size of the folder including subfolders (C:\Users\myuser\AppData\Local) is 27064 MB`
 
-Requirements:
-Python3
 
-`python ./src/start.py`
+Parameter:
 
-Just change the variables `scanpath` and `filepath` on the ./src/start.py script to fit your
-own demands.
+`start.py arg1=scanfolder arg2=Resultfile arg3=minimumscansize`
+
+`python ./src/start.py C:\\Users\\ D:\\filesizescan.txt 5`
+
+_Written in Python 3.9_
--- src/start.py
+++ src/start.py
@@ -1,30 +1,46 @@
+#!/usr/bin/python
 import DiskSizeUtility
 import DiskObject
+import sys
 
-# The target folder for scanning.
-scanpath = "C:\\Users\\"
-# The result file.
-filepath = "D:\\filesizescan.txt"
 
-# Filters out all files, that are smaller that the threshold.
-minimum_file_size_mb = 0
+def get_help():
+    print("start.py Scanfolder:[arg1] Resultfile[arg2] Minimum size in megabyte[arg3]")
+    print("Example: python ./src/start.py 'C:\\Users\\' 'D:\\filesizescan.txt'")
 
-disk_util = DiskSizeUtility.DiskSizeUtility(scanpath)
-print("Starting disk scan ...")
 
-# Invoke a new scan.
-disk_objects = disk_util.get_directories(minimum_file_size_mb)
+def run(scan_path, file_path, minimum_file_size_mb):
+    disk_util = DiskSizeUtility.DiskSizeUtility(scan_path)
+    print("Starting disk scan ...")
 
-print("Disk scan completed! Writing results to a file ..." + filepath)
+    # Invoke a new scan.
+    disk_objects = disk_util.get_directories(int(minimum_file_size_mb))
 
-f = open(filepath, "a", encoding="utf-8")
+    print("Disk scan completed! Writing results to a file ..." + file_path)
 
-for i in disk_objects:
-    if i.is_directory:
-        f.write(f"Size of the folder including sub folders ({i.object_path}) is {i.object_size} MB")
-    else:
-        f.write(f"Size of the file ({i.object_path}) is {i.object_size} MB")
-    f.write('\n')
-f.close()
+    f = open(file_path, "a", encoding="utf-8")
 
-print("The operation has been completed. Please check the result file: " + filepath)
+    for i in disk_objects:
+        if i.is_directory:
+            f.write(f"Size of the folder including sub folders ({i.object_path}) is {i.object_size} MB")
+        else:
+            f.write(f"Size of the file ({i.object_path}) is {i.object_size} MB")
+        f.write('\n')
+    f.close()
+
+    print("The operation has been completed. Please check the result file: " + file_path)
+
+
+if "-h" in sys.argv:
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    get_help()
+    sys.exit(0)
+
+if len(sys.argv) < 4:
+    print("Error: Three arguments required.")
+    get_help()
+    sys.exit(1)
+
+print(sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3])
+
+run(sys.argv[1], sys.argv[2], sys.argv[3])
